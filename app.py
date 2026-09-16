249eda622a4cb31372f4f39c3c21fb9a8bd3fe32
supitchaya-dear <supitchaya020819@gmail.com>
2026-09-16 08:05:41 +0000

    Add streamlit application

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-import pandas
-print('Hello World1!')
-print('This is a sample Python application.')
-df = pandas.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
-print('My first DataFrame')
-print(df)
+import pandas as pd
+import streamlit as st
+
+
+st.set_page_config(page_title="EDS2 Project")
+
+st.title("===== EDS2 Project =====")
+st.write("This is a sample Streamlit application.")
+
+df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
+
+st.subheader("My first DataFrame")
+st.dataframe(df, width="stretch", hide_index=True)
